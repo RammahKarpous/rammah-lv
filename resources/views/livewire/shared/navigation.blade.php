10f39b204d5ec618823eb3b34f05a8a476337e34
rammah karpous <rammahkarpous@outlook.com>
2024-02-14 14:41:21 +0100

    removed unneeded files

---
+++ resources/views/livewire/shared/navigation.blade.php
@@ -0,0 +1,3 @@
+<div class="z-10 p-6 sm:fixed sm:top-0 sm:right-0 text-end">
+    
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,3 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
-require __DIR__.'/auth.php';

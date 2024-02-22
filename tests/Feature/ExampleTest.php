cc7fcd4e132fb18e78ee5930246771fcf7168e5c
rammah karpous <rammahkarpous@outlook.com>
2024-02-22 18:09:55 +0100

    deleted all unneeded tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,10 @@
 
 it('returns a successful response', function () {
     $response = $this->get('/');
+    $response->assertStatus(200);
+});
 
+it('returns a successful articles page', function () {
+    $response = $this->get('/articles');
     $response->assertStatus(200);
 });

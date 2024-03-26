10b2257f13ecd3ab1446f1c5a57c02a02ab6c887
rammah karpous <rammahkarpous@outlook.com>
2024-03-26 11:05:16 +0100

    ran pa lang:publish command

---
+++ lang/nl/home.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return [
+    'header' => [
+        /**
+         * ---------------------------------
+         * Header section
+         * ---------------------------------
+         */
+        'page_title' => 'FULL STACK WEB DEVELOPER',
+        'content_title' => 'Hello there! 👋🏾',
+        'content_paragraphs' => [
+            "I'm Rammah Karpous, and my journey into web development began in 2016 during my second year of college. I started getting an interest into web development. I started with HTML and CSS. As my interest grew, I delved into PHP and JavaScript during my university years. This exploration led me to discover powerful frameworks like ReactJS and Laravel. This had shaped my passion for crafting innovative web solutions.",
+            
+            "In 2022, I took a significant step both professionally and geographically, relocating to the Netherlands. Joining the team at Frankwatching, I expanded my expertise to include NextJS, further refining my skills.",
+
+            "Now, as a co-founder of VioWebX Innovations, I bring years of experience and a diverse skill set to the table. VioWebX Innovations is as of now my side project, but in the future it hopefully will grow as a business offering creativity and technical excellence, offering cutting-edge web development solutions."
+        ],
+    ]
+];

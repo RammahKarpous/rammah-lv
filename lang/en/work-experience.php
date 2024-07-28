3388da51cb118b14d17a80e9805ba8309639be3e
rammah karpous <rammahkarpous@outlook.com>
2024-07-28 08:50:47 +0200

    feat: add De Indruk to my work-experience with brief description

--- lang/en/work-experience.php
+++ lang/en/work-experience.php
@@ -2,21 +2,30 @@
 
 return [
     [
+        "slug" => "de-indruk",
+        "title" => "De Indruk",
+        "period" => "Jul 2024 - Present",
+        "description" =>
+        "At De Indruk, I was involved in building websites for various clients.",
+
+        "tags" => ["Roots", "WordPress", "Laravel", "TailwindCSS"]
+    ],
+    [
         "slug" => "frankwatching",
         "title" => "Frankwatching",
         "period" => "Feb 2022 - Nov 2023",
-        "description" => 
-            "At Frankwatching, I was involved in building a new version of the existing platform. In a team of 4 web developers and 1 UI/UX designer, I was primarily responsible for implementing the functionalities and converting the design into code.",
+        "description" =>
+        "At Frankwatching, I was involved in building a new version of the existing platform. In a team of 4 web developers and 1 UI/UX designer, I was primarily responsible for implementing the functionalities and converting the design into code.",
 
-        "tags" => ["NextJS", "GraphQL", "ApolloJS", "Github Actions",]
+        "tags" => ["NextJS", "GraphQL", "ApolloJS", "Github Actions"]
     ],
     [
         "slug" => "kanuka-digital",
         "title" => "Kanuka Digital",
         "period" => "Nov 2019 - Mar 2020",
-        "description" => 
-            "During my studies, I worked part-time in a small team as a Junior WordPress Developer at a company called Kanuka Digital in Stafford. In that role, I was primarily involved in front-end development, using HTML, CSS, and JavaScript. Occasionally, I also had to modify PHP code to make certain elements appear.",
-        
+        "description" =>
+        "During my studies, I worked part-time in a small team as a Junior WordPress Developer at a company called Kanuka Digital in Stafford. In that role, I was primarily involved in front-end development, using HTML, CSS, and JavaScript. Occasionally, I also had to modify PHP code to make certain elements appear.",
+
         "tags" => ["HTML", "CSS", "BEM", "JavaScript", "WordPress", "ACF"]
     ],
 ];
--- lang/nl/work-experience.php
+++ lang/nl/work-experience.php
@@ -2,11 +2,20 @@
 
 return [
     [
+        "slug" => "de-indruk",
+        "title" => "De Indruk",
+        "period" => "Jul 2024 - Heden",
+        "description" =>
+        "Bij De Indruk houd ik me bezig met het bouwen van van website voor verschillende klanten.",
+        
+        "tags" => ["Roots", "WordPress", "Laravel", "TailwindCSS"]
+    ],
+    [
         "slug" => "frankwatching",
         "title" => "Frankwatching",
         "period" => "Feb 2022 - Nov 2023",
-        "description" => 
-            "Bij Frankwatching hield ik me bezig met het bouwen van een nieuwe versie van het bestaande platform. In een team van 4 webontwikkelaars en 1 UI/UX designer, was ik voornamelijk verantwoordelijk voor het implementeren van de functionaliteiten en het omzetten van het design naar code",
+        "description" =>
+        "Bij Frankwatching hield ik me bezig met het bouwen van een nieuwe versie van het bestaande platform. In een team van 4 webontwikkelaars en 1 UI/UX designer, was ik voornamelijk verantwoordelijk voor het implementeren van de functionaliteiten en het omzetten van het design naar code",
 
         "tags" => ["NextJS", "GraphQL", "ApolloJS", "Github Actions",]
     ],
@@ -14,9 +23,9 @@ return [
         "slug" => "kanuka-digital",
         "title" => "Kanuka Digital",
         "period" => "Nov 2019 - Maa 2020",
-        "description" => 
-            "Tijdens mijn studie werkte ik parttime in een klein team als Junior WordPress Developer bij een bedrijf genaamd Kanuka Digital in Stafford. In die functie was ik voornamelijk bezig met front- endontwikkeling, met HTML, CSS en JavaScript. Af en toe moest ik ook PHP-code aanpassen om bepaalde elementen te laten verschijnen.",
-        
+        "description" =>
+        "Tijdens mijn studie werkte ik parttime in een klein team als Junior WordPress Developer bij een bedrijf genaamd Kanuka Digital in Stafford. In die functie was ik voornamelijk bezig met front- endontwikkeling, met HTML, CSS en JavaScript. Af en toe moest ik ook PHP-code aanpassen om bepaalde elementen te laten verschijnen.",
+
         "tags" => ["HTML", "CSS", "BEM", "JavaScript", "WordPress", "ACF"]
     ],
 ];
